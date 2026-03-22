4cf7c9ff156db1739ac1be4df17df9ef0b1e0b33
Aaditya Kshatriya <aadityakshatriya002@gmail.com>
2026-03-22 16:55:20 +0530

    added a fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,14 +12,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #make a ship
+    #make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings= ai_settings, screen = screen)
-
-    #make a group to store bullets in
     bullets = Group()
+    aliens = Group()
 
-    #make an alien
-    alien = Alien(ai_settings,screen)
+    #create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
 
 #start the main loop for the game
     while True:
@@ -30,6 +29,6 @@ def run_game():
         gf.update_bullets(bullets)
 
         #update the screen with the newest frame
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
